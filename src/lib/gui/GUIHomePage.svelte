951cff85eb5cbc89cb9d2ffc0e2c8250cc784b1c
Shawn <changoloco.evg@gmail.com>
2025-07-20 22:09:18 +0000

    refactor: add GUIPostCard component

--- src/lib/gui/GUIHomePage.svelte
+++ src/lib/gui/GUIHomePage.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PostShape } from '@business/schema/PostSchema';
+	import GUIPostCard from '@gui/GUIPostCard.svelte';
 	import DateFormatter from '@common/DateFormatter';
 
 	export let data: { postList: PostShape[] };
@@ -16,21 +17,8 @@
 	<section>
 		<h2 class="sr-only">Recent Posts</h2>
 		<ul>
-			{#each data.postList as post}
-				<div class="grid gap-2 p-4 border-b border-neutral-900">
-					<div>
-						<p class="text-lg text-white font-medium">{post.author.name}</p>
-						<a class="text-sm text-neutral-400" href="/{post.author.displayName}">@{post.author.displayName}</a>
-					</div>
-					<p class="leading-relaxed">{post.content}</p>
-					{#if post.createdAt}
-						<time class="text-sm text-neutral-400" datetime={post.createdAt.toISOString()}>
-							{DateFormatter.getFormattedDate(post.createdAt)} - {DateFormatter.getRelativeDate(
-								post.createdAt
-							)}
-						</time>
-					{/if}
-				</div>
+			{#each data.postList as post (post.id)}
+				<GUIPostCard {post} />
 			{/each}
 		</ul>
 	</section>
---
+++ src/lib/gui/GUIPostCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { PostShape } from '@business/schema/PostSchema';
+	import DateFormatter from '@common/DateFormatter';
+
+	export let post: PostShape;
+</script>
+
+<div class="grid gap-2 p-4 border-b border-neutral-900">
+	<div>
+		<p class="text-lg text-white font-medium">{post.author.name}</p>
+		<a class="text-sm text-neutral-400" href="/{post.author.displayName}">
+			@{post.author.displayName}
+		</a>
+	</div>
+	<p class="leading-relaxed">{post.content}</p>
+	{#if post.createdAt}
+		<time class="text-sm text-neutral-400" datetime={post.createdAt.toISOString()}>
+			{DateFormatter.getFormattedDate(post.createdAt)} - {DateFormatter.getRelativeDate(
+				post.createdAt
+			)}
+		</time>
+	{/if}
+</div>
