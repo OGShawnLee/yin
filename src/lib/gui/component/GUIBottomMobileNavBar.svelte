733bf5c84aff469daaefdd1ecd8788def9a46631
Shawn <changoloco.evg@gmail.com>
2025-08-16 02:15:50 +0000

    fix(gui): add GUIBottomMobileNavBar labels

--- src/lib/gui/component/GUIBottomMobileNavBar.svelte
+++ src/lib/gui/component/GUIBottomMobileNavBar.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
 	import { CurrentUserState } from '@gui/State';
-	import {
-		Bell,
-		Bookmarks,
-		House,
-		MagnifyingGlass,
-		Pen,
-		SignIn,
-		SignOut,
-		User
-	} from 'phosphor-svelte';
-	import { enhance } from '$app/forms';
+	import { Bell, House, MagnifyingGlass, Pen, SignIn, User, UserCircle } from 'phosphor-svelte';
 
 	export let route: 'Home' | 'Profile' | 'Notifications' | 'Search' | 'Bookmarks' | 'None';
 
@@ -21,15 +11,16 @@
 	class="fixed bottom-0 inset-x-0 h-20 px-8 flex md:hidden items-center justify-between bg-black/70 backdrop-filter backdrop-blur-sm border-t-2 border-inactive"
 >
 	<a
-    class="size-12 min-w-12 flex items-center justify-center text-white"
-    href="/search"
-    aria-label="Search"
-  >
-    <MagnifyingGlass size={24} weight={route === 'Search' ? 'fill' : 'regular'} />
-    <p class="sr-only">Search</p>
-  </a>
+		class="size-12 min-w-12 flex flex-col items-center"
+		class:text-white={route === 'Search'}
+		href="/search"
+		aria-label="Search"
+	>
+		<MagnifyingGlass size={28} weight={route === 'Search' ? 'fill' : 'regular'} />
+		<span class="text-xs"> Search </span>
+	</a>
 	{#if $currentUser}
-	<!-- TODO: Add Mobile Sidebar -->
+		<!-- TODO: Add Mobile Sidebar -->
 		<!-- <form action="/auth/sign-out" method="post" use:enhance>
 			<button
 				class="size-12 min-w-12 flex items-center justify-center text-white"
@@ -40,19 +31,20 @@
 			</button>
 		</form> -->
 		<a
-			class="size-12 min-w-12 flex items-center justify-center text-white"
+			class="size-12 min-w-12 flex flex-col items-center"
+			class:text-white={route === 'Profile'}
 			href="/{$currentUser.displayName}"
 			aria-label="Profile"
 		>
-			<User size={24} weight={route === 'Profile' ? 'fill' : 'regular'} />
-			<p class="sr-only">Profile</p>
+			<UserCircle size={28} weight={route === 'Profile' ? 'fill' : 'regular'} />
+			<span class="text-xs"> Profile </span>
 		</a>
 		<a
-			class="size-12 min-w-12 flex items-center justify-center bg-main rounded-full text-white"
+			class="size-14 min-w-14 flex items-center justify-center bg-main rounded-full text-white"
 			href="/post/compose"
 			aria-label="Create Post"
 		>
-			<Pen size={24} />
+			<Pen size={32} />
 			<p class="sr-only">Create Post</p>
 		</a>
 		<!-- TODO: Add Mobile Sidebar -->
@@ -65,29 +57,31 @@
 			<p class="sr-only">Bookmarks</p>
 		</a> -->
 		<a
-			class="size-12 min-w-12 flex items-center justify-center text-white"
+			class="size-12 min-w-12 flex flex-col items-center"
+			class:text-white={route === 'Notifications'}
 			href="/notifications"
 			aria-label="Notifications"
 		>
-			<Bell size={24} weight={route === 'Notifications' ? 'fill' : 'regular'} />
-			<p class="sr-only">Notifications</p>
+			<Bell size={28} weight={route === 'Notifications' ? 'fill' : 'regular'} />
+			<span class="text-xs"> Activity </span>
 		</a>
 	{:else}
 		<a
-			class="size-12 min-w-12 flex items-center justify-center text-white"
+			class="size-12 min-w-12 flex flex-col items-center"
 			href="/auth/sign-in"
 			aria-label="Sign In"
 		>
-			<SignIn size={24} />
-			<p class="sr-only">Sign In</p>
+			<SignIn size={28} />
+			<span class="text-xs"> Sign In </span>
 		</a>
 	{/if}
 	<a
-		class="size-12 min-w-12 flex items-center justify-center text-white"
+		class="size-12 min-w-12 flex flex-col items-center"
+		class:text-white={route === 'Home'}
 		href="/"
 		aria-label="Home"
 	>
-		<House size={24} weight={route === 'Home' ? 'fill' : 'regular'} />
-		<p class="sr-only">Home</p>
+		<House size={28} weight={route === 'Home' ? 'fill' : 'regular'} />
+		<span class="text-xs"> Home </span>
 	</a>
 </nav>
